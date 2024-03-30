ab9369a7c931b07376cb1c463ec0283ebc666482
SabauEduard <sabaueduard@yahoo.com>
2024-03-30 20:24:14 +0200

    Added TestNFT contract to use when testing auctions

---
+++ contracts/TestNFT.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TestNFT is ERC721 {
+    uint256 private _tokenIds;
+    
+    constructor() ERC721("TestNFT", "TNFT") {
+        _tokenIds = 0;
+    }
+
+    function mint(address to) external {
+        _tokenIds++;
+        uint256 newTokenId = _tokenIds;
+        _mint(to, newTokenId);
+    }
+}
