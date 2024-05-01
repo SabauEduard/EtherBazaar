96d1248185cfe88d0f1737274a284ec12855812c
Robert <robert.udrea@cst.ro>
2024-05-01 16:03:47 +0300

    bid

--- frontend/src/components/Bazar.js
+++ frontend/src/components/Bazar.js
@@ -1,8 +1,28 @@
-import { Flex, Text, Button } from "@chakra-ui/react";
+import { Flex, Text, Button, Input } from "@chakra-ui/react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  bazcoinContract,
+  bazarContract,
+  nftContract,
+  provider,
+} from "../utils/ethersConnect";
 
 export const Bazar = () => {
   const nav = useNavigate();
+  const [bidValue, setBidValue] = useState("");
+
+  const handleBid = async () => {
+    const [owner, user1, user2] = await provider.listAccounts();
+    console.log(user1);
+    let tx = await bazcoinContract
+      .connect(user1)
+      .approve(bazarContract.target, parseInt(bidValue));
+    tx = await bazarContract.connect(user1).placeBid(0, parseInt(bidValue));
+
+    console.log("Bid successfully");
+  };
+
   return (
     <Flex
       flex={1}
@@ -16,6 +36,20 @@ export const Bazar = () => {
       </Text>
 
       <Text>Welcome to bazar</Text>
+
+      <Flex gap={5} width={"100%"} justify={"center"}>
+        <Button colorScheme="twitter" onClick={handleBid}>
+          Bid
+        </Button>
+        <Input
+          type="number"
+          width={"100px"}
+          value={bidValue}
+          onChange={(e) => {
+            setBidValue(e.target.value);
+          }}
+        />
+      </Flex>
 
       <Button
         onClick={() => {
